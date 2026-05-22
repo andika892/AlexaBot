a7c0087f2bb499f243f828ba5d6c0787e9e518d7
andika892 <154534226+andika892@users.noreply.github.com>
2026-05-23 00:13:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildVoiceStates
-  ]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates
+    ]
 });
 
 client.once("ready", () => {
